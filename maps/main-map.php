1f284e47b982f9d580b2c563c1a96a81d81e9855
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-02-08 09:58:41 -0200

    Initial commit

---
+++ maps/main-map.php
@@ -0,0 +1,9 @@
+<?php
+
+$file_name = 'main-map.png';
+echo "[MD5] = ". md5_file($file_name);
+echo "\n[DownloadLocation] = https://api.wynntils.com/maps/main-map.png";
+echo "\n[CenterX] = 1699";
+echo "\n[CenterZ] = -123";
+
+?>
