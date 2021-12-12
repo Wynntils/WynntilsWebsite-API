f345b84a0f73ec61fab05a84800ae9c9f87afa56
Magnus Ihse Bursie <mag@icus.se>
2021-12-12 15:32:21 +0000

    Offset the other way around...

--- maps/main-map.php
+++ maps/main-map.php
@@ -3,7 +3,7 @@
 $file_name = 'main-map.png';
 echo "[MD5] = ". md5_file($file_name);
 echo "\n[DownloadLocation] = https://api.wynntils.com/maps/main-map.png";
-echo "\n[CenterX] = 1708";
-echo "\n[CenterZ] = -88";
+echo "\n[CenterX] = 1690";
+echo "\n[CenterZ] = -158";
 
 ?>
