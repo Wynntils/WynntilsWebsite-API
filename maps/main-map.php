ceb26b7b607520fdcc844fdf99e6b6086db68b11
Magnus Ihse Bursie <mag@icus.se>
2021-12-12 15:27:09 +0000

    Change map center to match the new generated map

--- maps/main-map.php
+++ maps/main-map.php
@@ -3,7 +3,7 @@
 $file_name = 'main-map.png';
 echo "[MD5] = ". md5_file($file_name);
 echo "\n[DownloadLocation] = https://api.wynntils.com/maps/main-map.png";
-echo "\n[CenterX] = 1699";
-echo "\n[CenterZ] = -123";
+echo "\n[CenterX] = 1708";
+echo "\n[CenterZ] = -88";
 
 ?>
