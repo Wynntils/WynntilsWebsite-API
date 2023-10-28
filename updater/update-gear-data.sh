863cba9bdc4f675ea9a9e7125764e7b7eebf98f2
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-10-28 19:08:36 +0200

    chore: Update Item API to V3

--- updater/update-gear-data.sh
+++ updater/update-gear-data.sh
@@ -6,7 +6,7 @@ TARGET_DIR=$(cd $(dirname "$0")/.. >/dev/null 2>&1 && pwd)
 cd $TARGET_DIR
 
 # Download the json file from Wynncraft API
-wget -O gear.json.tmp "https://api.wynncraft.com/public_api.php?action=itemDB&category=all"
+curl -X POST -d '{"type":["weapons","armour","accessories"]}' -H "Content-Type: application/json" -o gear.json.tmp "https://api.wynncraft.com/v3/item/search?fullResult=True"
 
 if [ ! -s gear.json.tmp ]; then
     rm gear.json.tmp
@@ -16,9 +16,9 @@ fi
 
 # Sort the items and keys in the json file, since the Wynncraft API is not stable in its order
 # This will also get rid of the timestamp, which would mess up the md5sum
-jq --sort-keys '{"items":  .items | sort_by(.name)}' < gear.json.tmp > gear.json.tmp2
-# Delete zero and empty values to keep size down and readability up
-jq -c 'del(..|select(. == 0 or . == null or . == "0-0"))' < gear.json.tmp2 > gear.json
+jq --sort-keys -r '.' < gear.json.tmp > gear.json.tmp2
+# Minimalize the json file
+jq -c < gear.json.tmp2 > gear.json
 rm gear.json.tmp gear.json.tmp2
 
 # To be able to review new data, we also need an expanded, human-readable version
@@ -27,6 +27,6 @@ jq '.' < gear.json > gear_expanded.json
 # Calculate md5sum of the new gear data
 MD5=$(md5sum $TARGET_DIR/gear.json | cut -d' ' -f1)
 
-# Update ulrs.json with the new md5sum for dataStaticGear
-jq '. = [.[] | if (.id == "dataStaticGear") then (.md5 = "'$MD5'") else . end]' < urls.json > urls.json.tmp
+# Update ulrs.json with the new md5sum for dataStaticGearAdvanced
+jq '. = [.[] | if (.id == "dataStaticGearAdvanced") then (.md5 = "'$MD5'") else . end]' < urls.json > urls.json.tmp
 mv urls.json.tmp urls.json
--- updater/update-ingredient-data.sh
+++ updater/update-ingredient-data.sh
@@ -6,7 +6,7 @@ TARGET_DIR=$(cd $(dirname "$0")/.. >/dev/null 2>&1 && pwd)
 cd $TARGET_DIR
 
 # Download the json file from Wynncraft API
-wget -O ingredients.json.tmp "https://api.wynncraft.com/v2/ingredient/search/skills/%5Etailoring,armouring,jeweling,cooking,woodworking,weaponsmithing,alchemism,scribing"
+curl -X POST -d '{"type":["ingredients"]}' -H "Content-Type: application/json" -o ingredients.json.tmp "https://api.wynncraft.com/v3/item/search?fullResult=True"
 
 if [ ! -s ingredients.json.tmp ]; then
     rm ingredients.json.tmp
@@ -16,9 +16,9 @@ fi
 
 # Sort the items and keys in the json file, since the Wynncraft API is not stable in its order
 # This will also get rid of the timestamp, which would mess up the md5sum
-jq --sort-keys '{"ingredients":  .data | sort_by(.name)}' < ingredients.json.tmp > ingredients.json.tmp2
-# Delete zero and empty values, and then objects that get empty, to keep size down and readability up
-jq -c 'del(..|select(. == 0 or . == null)) | del(..|select( . == {}))' < ingredients.json.tmp2 > ingredients.json
+jq --sort-keys < ingredients.json.tmp > ingredients.json.tmp2
+# Minimalize the json file
+jq -c < ingredients.json.tmp2 > ingredients.json
 rm ingredients.json.tmp ingredients.json.tmp2
 
 # To be able to review new data, we also need an expanded, human-readable version
@@ -27,6 +27,6 @@ jq '.' < ingredients.json > ingredients_expanded.json
 # Calculate md5sum of the new ingredient data
 MD5=$(md5sum $TARGET_DIR/ingredients.json | cut -d' ' -f1)
 
-# Update ulrs.json with the new md5sum for dataStaticIngredients
-jq '. = [.[] | if (.id == "dataStaticIngredients") then (.md5 = "'$MD5'") else . end]' < urls.json > urls.json.tmp
+# Update ulrs.json with the new md5sum for dataStaticIngredientsAdvanced
+jq '. = [.[] | if (.id == "dataStaticIngredientsAdvanced") then (.md5 = "'$MD5'") else . end]' < urls.json > urls.json.tmp
 mv urls.json.tmp urls.json
